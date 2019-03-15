5fab488a5d062ced2016607d1e8feb2fd8d82951
Lucas Martins Domingues <lucas.progmd@gmail.com>
2019-03-15 10:14:53 -0300

    commit

--- types.go
+++ types.go
@@ -36,15 +36,15 @@ type Weather struct {
 }
 
 type WeatherData struct {
-	Temperature   int64  `json:"temperature"`
-	WindDirection string `json:"wind_direction"`
-	WindVelocity  int64  `json:"wind_velocity"`
-	Humidity      int64  `json:"humidity"`
-	Condition     string `json:"condition"`
-	Pressure      int64  `json:"pressure"`
-	Icon          string `json:"icon"`
-	Sensation     int64  `json:"sensation"`
-	Date          string `json:"date"`
+	Temperature   int64   `json:"temperature"`
+	WindDirection string  `json:"wind_direction"`
+	WindVelocity  float64 `json:"wind_velocity"`
+	Humidity      float64 `json:"humidity"`
+	Condition     string  `json:"condition"`
+	Pressure      float64 `json:"pressure"`
+	Icon          string  `json:"icon"`
+	Sensation     int64   `json:"sensation"`
+	Date          string  `json:"date"`
 }
 
 type ClimaTempoError struct {
